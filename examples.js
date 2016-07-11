6f572de038be9204bcd86a4edf248d34fac013e0
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-11 08:25:20 -0400

    examples

--- examples.js
+++ examples.js
@@ -5,9 +5,11 @@ var utils = require('./');
 for (var key in utils) {
   var val = utils[key];
   if (typeof val === 'string') {
-    console.log(val);
+    console.log(val, `(${key})`);
   }
 }
 
+// console.log(utils.heading('foo', 'bar'));
 // console.log(utils.timestamp);
-// console.log(utils.success);
+// console.log(utils.symbol);
+utils.ok('   foo');
